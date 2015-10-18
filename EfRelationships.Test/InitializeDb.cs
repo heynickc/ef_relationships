510145ddfdc99a76093659b46d82c88bdd83188a
Nick Chamberlain <nick.chamberlain.jr@gmail.com>
2015-10-17 22:36:50 -0400

    First commit

---
+++ EfRelationships.Test/InitializeDb.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace EfRelationships.Test {
+
+    public class InitializeDb {
+        [Fact]
+        public void SeedEmailsTable() {
+            using (EfRelationshipsDbContext db = new EfRelationshipsDbContext()) {
+                Email email = new Email() {
+                    Subject = "Hello",
+                    Body = "Here's some work for you to derail you from your project entirely!",
+                    FromAddress = "test@test.com",
+                    //ToAddresses = new List<EmailAddressMap> {
+                    //    new EmailAddressMap() {
+                    //        EmailAddress = new EmailAddress() {
+                    //            Address = "test@test.com" },
+                    //    },
+                    //    new EmailAddressMap() {
+                    //        EmailAddress = new EmailAddress() {
+                    //            Address = "test@test.com" },
+                    //    },
+                    //    new EmailAddressMap() {
+                    //        EmailAddress = new EmailAddress() {
+                    //            Address = "test@test.com" },
+                    //    },
+                    //},
+                    //CcAddresses = new List<EmailAddressMap> {
+                    //    new EmailAddressMap() {
+                    //        EmailAddress = new EmailAddress() {
+                    //            Address = "test@test.com" },
+                    //    },
+                    //},
+                    //BccAddresses = new List<EmailAddressMap> {
+                    //    new EmailAddressMap() {
+                    //        EmailAddress = new EmailAddress() {
+                    //            Address = "test@test.com" },
+                    //    },
+                    //},
+                    ToAddresses = new List<EmailAddressMap> {
+                        new EmailAddressMap() {
+                            EmailAddress = new EmailAddress() {
+                                Address = "test@test.com" },
+                                EmailAddressType = EmailAddressType.To
+                        },
+                        new EmailAddressMap() {
+                            EmailAddress = new EmailAddress() {
+                                Address = "test@test.com" },
+                                EmailAddressType = EmailAddressType.To
+                        },
+                        new EmailAddressMap() {
+                            EmailAddress = new EmailAddress() {
+                                Address = "test@test.com" },
+                                EmailAddressType = EmailAddressType.Cc
+                        },
+                        new EmailAddressMap() {
+                            EmailAddress = new EmailAddress() {
+                                Address = "test@test.com" },
+                                EmailAddressType = EmailAddressType.Bcc
+                        },
+                        new EmailAddressMap() {
+                            EmailAddress = new EmailAddress() {
+                                Address = "test@test.com" },
+                                EmailAddressType = EmailAddressType.Bcc
+                        },
+                        new EmailAddressMap() {
+                            EmailAddress = new EmailAddress() {
+                                Address = "test@test.com" },
+                                EmailAddressType = EmailAddressType.Bcc
+                        },
+                    }
+                };
+
+                db.Emails.Add(email);
+                db.SaveChanges();
+            }
+        }
+
+        [Fact]
+        public void SendAnEmail() {
+            using (EfRelationshipsDbContext db = new EfRelationshipsDbContext()) {
+
+                var email = db.Emails.Where(e => e.ID == 1).FirstOrDefault();
+                var toAddresses = email.ToAddresses
+                    .Where(x => x.EmailAddressType == EmailAddressType.To)
+                    .Select(y => new MailAddress(y.EmailAddress.Address))
+                    .Select(z => new MailAddressCollection() { z }).FirstOrDefault();
+                var ccAddresses = email.ToAddresses
+                    .Where(x => x.EmailAddressType == EmailAddressType.Cc)
+                    .Select(y => new MailAddress(y.EmailAddress.Address))
+                    .Select(z => new MailAddressCollection() { z }).FirstOrDefault();
+                var bccAddresses = email.ToAddresses
+                    .Where(x => x.EmailAddressType == EmailAddressType.Bcc)
+                    .Select(y => new MailAddress(y.EmailAddress.Address))
+                    .Select(z => new MailAddressCollection() { z }).FirstOrDefault();
+
+                var emailMessage = new MailMessage() {
+                    Subject = email.Subject,
+                    Body = email.Body,                    
+                };
+
+                foreach (var address in toAddresses) {
+                    emailMessage.To.Add(address);
+                }
+                foreach (var address in ccAddresses) {
+                    emailMessage.CC.Add(address);
+                }
+                foreach (var address in bccAddresses) {
+                    emailMessage.Bcc.Add(address);
+                }
+            }
+        }
+    }
+}
---
+++ EfRelationships/Email.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EfRelationships {
+    public enum EmailAddressType {
+        To,
+        Cc,
+        Bcc
+    }
+
+    [Table("Emails")]
+    public class Email {
+        [Key]
+        public int ID { get; set; }
+        public string Subject { get; set; }
+        public string Body { get; set; }
+        public string FromAddress { get; set; }
+        public virtual ICollection<EmailAddressMap> ToAddresses { get; set; }
+    }
+    
+    [Table("EmailAddressMap")]
+    public class EmailAddressMap {
+        [Key]
+        public int ID { get; set; }
+        public EmailAddress EmailAddress { get; set; }
+        public EmailAddressType EmailAddressType { get; set; }
+    }
+
+    [Table("EmailAddresses")]
+    public class EmailAddress {
+        [Key]
+        public int ID { get; set; }
+        public string Address { get; set; }
+    }
+
+    public class EfRelationshipsDbContext : DbContext {
+        public EfRelationshipsDbContext() : base("name=EfRelationshipsDb") {
+            Database.SetInitializer<EfRelationshipsDbContext>(new DropCreateDatabaseAlways<EfRelationshipsDbContext>());
+        }
+        public virtual DbSet<Email> Emails { get; set; }
+        public virtual DbSet<EmailAddressMap> EmailAddressMaps { get; set; }
+        public virtual DbSet<EmailAddress> EmailAddresses { get; set; }
+    }
+}
